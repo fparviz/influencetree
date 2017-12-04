d3afa5a88e3be3ca5b69675d7b0b3c1c16241c52
Fyza <fyza.parviz@gmail.com>
2017-12-04 15:49:14 -0800

    all the latest changes

---
+++ region1.js
@@ -0,0 +1,388 @@
+var region =
+
+	{
+		"name": "United States",
+		"children": [{
+			"name": "Seattle"
+			},
+			{
+			"name": "Minnesota"
+			},
+			{
+			"name":"United States"
+			},
+			{
+			"name":"Minneapolis"
+			},
+			{
+			"name":"Ann Arbor Michigan"
+			},
+			{
+			"name":"Michigan"
+			},
+			{
+			"name":"Brooklyn"
+			},
+			{
+			"name":"New York"
+			},
+			{
+			"name":"Indianapolis"
+			},
+			{
+			"name":"New York City"
+			},
+			{
+			"name":"Ossining (village)"
+			},
+			{
+			"name":"Bronxville New York"
+			},
+			{
+			"name":"Ohio"
+			},
+			{
+			"name":"Cincinnati"
+			},
+			{
+			"name":"Detroit"
+			},
+			{
+			"name":"Milwaukee"
+			},
+			{
+			"name":"Salt Lake City"
+			},
+			{
+			"name":"Montana"
+			},
+			{
+			"name":"Adams New York"
+			},
+			{
+			"name":"Saginaw Michigan"
+			},
+			{
+			"name":"White Plains New York"
+			},
+			{
+			"name":"North Attleborough Massachusetts"
+			},
+			{
+			"name":"Spokane Washington"
+			},
+			{
+			"name":"Washington (state)"
+			},
+			{
+			"name":"Newark New Jersey"
+			},
+			{
+			"name":"New Jersey"
+			},
+			{
+			"name":"Union Hill New Jersey"
+			},
+			{
+			"name":"Mendham Borough New Jersey"
+			},
+			{
+			"name":"Endicott New York"
+			},
+			{
+			"name":"Grand Forks North Dakota"
+			},
+			{
+			"name":"Massachusetts"
+			},
+			{
+			"name":"Boston"
+			},
+			{
+			"name":"Stoneham Massachusetts"
+			},
+			{
+			"name":"Chicago"
+			},
+			{
+			"name":"Illinois"
+			},
+			{
+			"name":"Groton Massachusetts"
+			},
+			{
+			"name":"Los Angeles"
+			},
+			{
+			"name":"Bigelow Minnesota"
+			},
+			{
+			"name":"Minnesota"
+			},
+			{
+			"name":"Somerville Massachusetts"
+			},
+			{
+			"name":"Bethesda Maryland"
+			},
+			{
+			"name":"Washington D.C."
+			},
+			{
+			"name":"Waukegan Illinois"
+			},
+			{
+			"name":"Galesburg Illinois"
+			},
+			{
+			"name":"Peoria Illinois"
+			},
+			{
+			"name":"Portsmouth New Hampshire"
+			},
+			{
+			"name":"Smith County Mississippi"
+			},
+			{
+			"name":"Mississippi"
+			},
+			{
+			"name":"Akron Ohio"
+			},
+			{
+			"name":"Fredericksburg Ohio"
+			},
+			{
+			"name":"Oberlin Ohio"
+			},
+			{
+			"name":"Delaware"
+			},
+			{
+			"name":"Glendale Ohio"
+			},
+			{
+			"name":"Glendale California"
+			},
+			{
+			"name":"Long Beach California"
+			},
+			{
+			"name":"Grass Valley California"
+			},
+			{
+			"name":"Fontana California"
+			},
+			{
+			"name":"Flint Michigan"
+			},
+			{
+			"name":"Manhattan Montana"
+			},
+			{
+			"name":"Atlanta"
+			},
+			{
+			"name":"Georgia (U.S. state)"
+			},
+			{
+			"name":"Bay Shore New York"
+			},
+			{
+			"name":"Rhode Island"
+			},
+			{
+			"name":"Providence Rhode Island"
+			},
+			{
+			"name":"Paterson New Jersey"
+			},
+			{
+			"name":"Scarsdale New York"
+			},
+			{
+			"name":"North Dakota"
+			},
+			{
+			"name":"Manhattan"
+			},
+			{
+			"name":"Cleveland"
+			},
+			{
+			"name":"Greene County Indiana"
+			},
+			{
+			"name":"Indiana"
+			},
+			{
+			"name":"Terre Haute Indiana"
+			},
+			{
+			"name":"Evansville Indiana"
+			},
+			{
+			"name":"Mays Indiana"
+			},
+			{
+			"name":"Oklahoma"
+			},
+			{
+			"name":"Tulsa Oklahoma"
+			},
+			{
+			"name":"Oklahoma City"
+			},
+			{
+			"name":"Miami"
+			},
+			{
+			"name":"Concord Massachusetts"
+			},
+			{
+			"name":"Columbus Georgia"
+			},
+			{
+			"name":"Utah"
+			},
+			{
+			"name":"Provo Utah"
+			},
+			{
+			"name":"Takoma Park Maryland"
+			},
+			{
+			"name":"Maryland"
+			},
+			{
+			"name":"Silver Spring Maryland"
+			},
+			{
+			"name":"Baltimore"
+			},
+			{
+			"name":"Burlington Vermont"
+			},
+			{
+			"name":"Colorado"
+			},
+			{
+			"name":"Denver"
+			},
+			{
+			"name":"Spring Green Wisconsin"
+			},
+			{
+			"name":"Pardeeville Wisconsin"
+			},
+			{
+			"name":"Pennsylvania"
+			},
+			{
+			"name":"Philadelphia"
+			},
+			{
+			"name":"Cherokee Iowa"
+			},
+			{
+			"name":"Iowa City Iowa"
+			},
+			{
+			"name":"Iowa"
+			},
+			{
+			"name":"Keokuk Iowa"
+			},
+			{
+			"name":"Hartford Connecticut"
+			},
+			{
+			"name":"Mud Lake Idaho"
+			},
+			{
+			"name":"Idaho"
+			},
+			{
+			"name":"Selden Kansas"
+			},
+			{
+			"name":"Kansas"
+			},
+			{
+			"name":"Hope Kansas"
+			},
+			{
+			"name":"Springfield Massachusetts"
+			},
+			{
+			"name":"South Hadley Massachusetts"
+			},
+			{
+			"name":"Portsmouth Virginia"
+			},
+			{
+			"name":"Rochester New York"
+			},
+			{
+			"name":"Worcester New York"
+			},
+			{
+			"name":"Schenectady New York"
+			},
+			{
+			"name":"Johnston Rhode Island"
+			},
+			{
+			"name":"Salem Oregon"
+			},
+			{
+			"name":"Langhorne Pennsylvania"
+			},
+			{
+			"name":"Texas"
+			},
+			{
+			"name":"Hereford Texas"
+			},
+			{
+			"name":"Guadalupe County Texas"
+			},
+			{
+			"name":"Scottsbluff Nebraska"
+			},
+			{
+			"name":"Shreveport Louisiana"
+			},
+			{
+			"name":"Papillion Nebraska"
+			},
+			{
+			"name":"Buffalo Missouri"
+			},
+			{
+			"name":"St. Louis"
+			},
+			{
+			"name":"Smicksburg Pennsylvania"
+			},
+			{
+			"name":"Duluth Minnesota"
+			},
+			{
+			"name":"Albuquerque New Mexico"
+			},
+			{
+			"name":"New England"
+			},
+			{
+			"name":"Norwich Vermont"
+			},
+			{
+			"name":"Vermont"
+			},
+			{
+			"name":"Erskine Minnesota"
+			}
+		]
+
+		}
